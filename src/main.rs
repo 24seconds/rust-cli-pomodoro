d20bbe48595a43a9bc227a5f9a6390dbdbce83ca
Young <24crazyoung@gmail.com>
2021-10-02 16:57:46 +0900

    Implement simple read from stdin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, Write};
+
+#[tokio::main]
+async fn main() {
+    loop {
+        println!("try to read command");
+        let command = read_command();
+        println!("user input: {}", command)
+    }
+}
+
+fn read_command() -> String {
+    print!("> ");
+
+    // ignore error
+    let _ = io::stdout().flush().unwrap();
+
+    let mut command = String::new();
+
+    io::stdin()
+        .read_line(&mut command)
+        .expect("Failed to read line");
+
+    let command = command.trim().to_string();
+
+    command
 }
