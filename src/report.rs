847d72d6374d4ccee45e151f860e275d3718f8f5
Young <24crazyoung@gmail.com>
2022-05-13 00:42:31 +0900

    move report function to report module

--- src/report.rs
+++ src/report.rs
@@ -1,8 +1,8 @@
 use colored::{ColoredString, Colorize};
-use std::error::Error;
 use tabled::{Style, Table, Tabled};
 
 use crate::error::ConfigurationError;
+use crate::error::NotifyResult;
 use crate::Configuration;
 
 #[derive(Tabled)]
@@ -22,7 +22,7 @@ impl Report {
     }
 
     // TODO(young): e should be string or ConfigurationError?
-    pub fn update_reason(mut self, e: ConfigurationError) -> Self {
+    pub fn update_reason(mut self, e: &dyn std::error::Error) -> Self {
         let mut vec = vec![format!("{}", e)];
         if let Some(s) = e.source() {
             vec.push(s.to_string());
@@ -39,28 +39,27 @@ pub fn generate_configuration_report(
     err: Option<ConfigurationError>,
 ) -> String {
     let config_err_message = match err {
-        Some(e) => Report::new("X", "no config err").update_reason(e),
+        Some(e) => Report::new("X", "no config err").update_reason(&e),
         None => Report::new("O", "no config err"),
     };
 
     let slack_channel_message = match config.get_slack_channel() {
         Some(_) => Report::new("O", "slack_channel"),
-        None => {
-            Report::new("X", "slack_channel").update_reason(ConfigurationError::SlackConfigNotFound)
-        }
+        None => Report::new("X", "slack_channel")
+            .update_reason(&ConfigurationError::SlackConfigNotFound),
     };
 
     let slack_token_message = match config.get_slack_channel() {
         Some(_) => Report::new("O", "slack_token"),
         None => {
-            Report::new("X", "slack_token").update_reason(ConfigurationError::SlackConfigNotFound)
+            Report::new("X", "slack_token").update_reason(&ConfigurationError::SlackConfigNotFound)
         }
     };
 
     let discord_webhook_url_message = match config.get_discord_webhook_url() {
         Some(_) => Report::new("O", "discord_webhook_url"),
         None => Report::new("X", "discord_webhook_url")
-            .update_reason(ConfigurationError::DiscordConfigNotFound),
+            .update_reason(&ConfigurationError::DiscordConfigNotFound),
     };
 
     Table::new(vec![
@@ -71,4 +70,29 @@ pub fn generate_configuration_report(
     ])
     .with(Style::modern())
     .to_string()
+}
+
+pub fn generate_notify_report(
+    desktop: NotifyResult,
+    slack: NotifyResult,
+    discord: NotifyResult,
+) -> String {
+    let desktop_message = match desktop {
+        Ok(_) => Report::new("O", "Desktop"),
+        Err(e) => Report::new("X", "Desktop").update_reason(&e),
+    };
+
+    let slack_message = match slack {
+        Ok(_) => Report::new("O", "Slack"),
+        Err(e) => Report::new("X", "Slack").update_reason(&e),
+    };
+
+    let discord_message = match discord {
+        Ok(_) => Report::new("O", "Discord"),
+        Err(e) => Report::new("X", "Discord").update_reason(&e),
+    };
+
+    Table::new(vec![desktop_message, slack_message, discord_message])
+        .with(Style::modern())
+        .to_string()
 }
