31eb36256ef52160632c0990ed4ca46e1d300bf7
Young <24crazyoung@gmail.com>
2023-04-05 22:17:34 +0900

    write down exsisting test cases in table format

--- src/notification/mod.rs
+++ src/notification/mod.rs
@@ -491,40 +491,154 @@ mod tests {
         path.push("resources/test/mock_configuration.json");
         let path_str = path.to_str().unwrap();
 
-        let (configuration, _) = load_configuration(Some(path_str)).unwrap();
+        let (mock_configuration, _) = load_configuration(Some(path_str)).unwrap();
+        let (empty_configuration, _) =
+            load_configuration(Some("this_path_does_not_exist")).unwrap();
+
         let mut id_manager = 0;
         let now = Utc::now();
 
         struct TestCase<'a> {
             desc: &'static str,
             command: &'static str,
+            use_mock_configuration: bool,
+            expect_notification_error: bool,
             expected_work_time: u16,
             expected_break_time: u16,
             expected_created_at: &'a DateTime<Utc>,
         }
 
-        let test_cases: Vec<TestCase> = vec![TestCase {
-            desc: "default flags with configuration",
-            command: "myapp --default",
-            expected_work_time: 30,
-            expected_break_time: 10,
-            expected_created_at: &now,
-        }];
+        let test_cases: Vec<TestCase> = vec![
+            TestCase {
+                desc: "default flags with configuration",
+                command: "myapp --default",
+                use_mock_configuration: true,
+                expect_notification_error: false,
+                expected_work_time: 30,
+                expected_break_time: 10,
+                expected_created_at: &now,
+            },
+            TestCase {
+                desc: "default flags without configuration",
+                command: "myapp --default",
+                use_mock_configuration: false,
+                expect_notification_error: false,
+                expected_work_time: 25,
+                expected_break_time: 5,
+                expected_created_at: &now,
+            },
+            TestCase {
+                desc: "no flags with configuration",
+                command: "myapp",
+                use_mock_configuration: true,
+                expect_notification_error: false,
+                expected_work_time: 30,
+                expected_break_time: 10,
+                expected_created_at: &now,
+            },
+            TestCase {
+                desc: "no flags without configuration",
+                command: "myapp",
+                use_mock_configuration: false,
+                expect_notification_error: false,
+                expected_work_time: 25,
+                expected_break_time: 5,
+                expected_created_at: &now,
+            },
+            TestCase {
+                desc: "both work and break flag specified with configuration",
+                command: "myapp -w 50 -b 25",
+                use_mock_configuration: true,
+                expect_notification_error: false,
+                expected_work_time: 50,
+                expected_break_time: 25,
+                expected_created_at: &now,
+            },
+            TestCase {
+                desc: "only work flag specified with configuration",
+                command: "myapp -w 50",
+                use_mock_configuration: true,
+                expect_notification_error: false,
+                expected_work_time: 50,
+                expected_break_time: 10,
+                expected_created_at: &now,
+            },
+            TestCase {
+                desc: "only break flag specified with configuration",
+                command: "myapp -b 25",
+                use_mock_configuration: true,
+                expect_notification_error: false,
+                expected_work_time: 30,
+                expected_break_time: 25,
+                expected_created_at: &now,
+            },
+            TestCase {
+                desc: "both work and break flag specified without configuration",
+                command: "myapp -w 50 -b 25",
+                use_mock_configuration: false,
+                expect_notification_error: false,
+                expected_work_time: 50,
+                expected_break_time: 25,
+                expected_created_at: &now,
+            },
+            TestCase {
+                desc: "only work flag specified without configuration",
+                command: "myapp -w 50",
+                use_mock_configuration: false,
+                expect_notification_error: false,
+                expected_work_time: 50,
+                expected_break_time: 5,
+                expected_created_at: &now,
+            },
+            TestCase {
+                desc: "only break flag specified without configuration",
+                command: "myapp -b 25",
+                use_mock_configuration: false,
+                expect_notification_error: false,
+                expected_work_time: 25,
+                expected_break_time: 25,
+                expected_created_at: &now,
+            },
+            TestCase {
+                desc: "both work and break are specified as zero with configuration",
+                command: "myapp -w 0 -b 0",
+                use_mock_configuration: true,
+                expect_notification_error: true,
+                expected_work_time: 0,
+                expected_break_time: 0,
+                expected_created_at: &now,
+            },
+            TestCase {
+                desc: "both work and break are specified as zero without configuration",
+                command: "myapp -w 0 -b 0",
+                use_mock_configuration: false,
+                expect_notification_error: true,
+                expected_work_time: 0,
+                expected_break_time: 0,
+                expected_created_at: &now,
+            },
+        ];
 
         for (idx, tc) in test_cases.iter().enumerate() {
             let cmd = Command::new("myapp");
             let matches =
                 add_args_for_create_subcommand(cmd).get_matches_from(tc.command.split_whitespace());
 
-            let notification = get_new_notification(
-                &matches,
-                &mut id_manager,
-                now.clone(),
-                Arc::new(configuration.clone()),
-            )
-            .unwrap();
+            let configuration = if tc.use_mock_configuration {
+                Arc::new(mock_configuration.clone())
+            } else {
+                Arc::new(empty_configuration.clone())
+            };
 
-            let (id, _, work_time, break_time, created_at, _, _) = notification.get_values();
+            let notification =
+                get_new_notification(&matches, &mut id_manager, now.clone(), configuration);
+
+            if tc.expect_notification_error {
+                assert!(notification.is_err(), "Test case: {}", tc.desc);
+                continue;
+            }
+
+            let (id, _, work_time, break_time, created_at, _, _) = notification.unwrap().get_values();
             assert_eq!(idx as u16, id, "Test case: {}", tc.desc);
             assert_eq!(tc.expected_work_time, work_time, "Test case: {}", tc.desc);
             assert_eq!(tc.expected_break_time, break_time, "Test case: {}", tc.desc);
